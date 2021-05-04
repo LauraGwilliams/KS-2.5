a4a14de7f15829625c683dd7b659c8172b2b37f0
Laura Gwilliams <laura.gwilliams@ucsf.edu>
2021-05-04 09:17:35 -0700

    add preproc.m

---
+++ preproc.m
@@ -0,0 +1,390 @@
+%% define paths
+
+clear all; clc;
+
+addpath(genpath('/userdata/lgwilliams/neuropixels/software/npy-matlab-master/npy-matlab/'))
+addpath(genpath('/userdata/lgwilliams/neuropixels/software/')) % path to kilosort folder
+addpath(genpath('/userdata/lgwilliams/neuropixels/')) % path to kilosort folder
+rootZ = '/userdata/lgwilliams/neuropixels/data/NP04/raw/NP04_B2_g0/NP04_B2_g0_imec0/'; % the raw data binary file is in this folder
+rootH = rootZ; % path to temporary binary file (same size as data, should be on fast SSD)
+pathToYourConfigFile = '/userdata/lgwilliams/neuropixels/scripts/configFiles'; % take from Github folder and put it somewhere else (together with the master_file)
+chanMapFile = 'neuropixPhase3B2_kilosortChanMap.mat';
+
+%% params
+ops.trange    = [240 840]; % time range to sort
+ops.NchanTOT  = 385; % total number of channels in your recording
+
+% filtering
+%ops.fslow = 10000;
+%ops.fshigh = 300;
+
+%% deriv paths
+run(fullfile(pathToYourConfigFile, 'configFile384.m'))
+ops.fproc   = fullfile(rootH, 'temp_wh.dat'); % proc file on a fast SSD
+ops.chanMap = fullfile(pathToYourConfigFile, chanMapFile);
+
+%% load params
+
+% load the channel map
+load(ops.chanMap);
+
+% load CORRECTED! event structure
+%events = load(fullfile(rootZ, 'NP04_evnt.mat'));
+%evnt = events.evnt;
+evnt = table2struct(readtable(fullfile(rootZ, 'NP04_evnt_CORRECTED.csv')));
+
+% load the true channel ordering
+ch_struct = load(fullfile(rootZ, 'NP04_chMap.mat'));
+value_cut = ch_struct.chMap(find(connected == 0));
+
+% which channels we use are defined in the physical space of the probe, not
+% the chronological order of the data
+bad_idx = find(ch_struct.chMap < 10 | ch_struct.chMap > 320);
+connected(bad_idx) = 0;
+connected(find(ch_struct.chMap == 186)) = 0;
+connected(find(ch_struct.chMap == 187)) = 0;
+ops.connected = connected;
+
+%channel_reorderer = ch_struct.chMap(connected);  % use boolean to subset
+
+% edit the 'connected' object to remove the bad channels from the
+% preprocessing
+%connected(1:10) = 0;
+%connected(320:384) = 0;  % 342:384
+%ops.connected = connected;
+
+%% this block runs all the steps of the algorithm
+fprintf('Looking for data inside %s \n', rootZ)
+
+% main parameter changes from Kilosort2 to v2.5
+ops.sig        = 20;  % spatial smoothness constant for registration
+ops.fshigh     = 300; % high-pass more aggresively
+ops.nblocks    = 5; % blocks for registration. 0 turns it off, 1 does rigid registration. Replaces "datashift" option.
+
+% is there a channel map file in this folder?
+fs = dir(fullfile(rootZ, 'chan*.mat'));
+if ~isempty(fs)
+    ops.chanMap = fullfile(rootZ, fs(1).name);
+end
+
+% find the binary file
+fs = [dir(fullfile(rootZ, '*.bin')) dir(fullfile(rootZ, '*df.dat'))];
+ops.fbinary = fullfile(rootZ, fs(1).name);
+
+% preprocess data to create temp_wh.dat
+sfreq = 30000;
+decimate = 1;
+new_sfreq = sfreq/decimate;
+
+% change the NT to be an integer of the resulting sfreq
+ops.NT = sfreq*2;
+
+% get data
+all_data_ds = preprocessData_returnMatrix(ops);
+
+%% load anin
+anin_path = '/userdata/lgwilliams/neuropixels/data/NP04/raw/NP04_B2_g0';
+aninBin_fname = 'NP04_B2_g0_t0.nidq.bin';
+aninMeta_fname = 'NP04_B2_g0_t0.nidq.meta';
+
+% load it
+aninmeta = ReadMeta(aninMeta_fname, anin_path);
+
+% Get first one second of data
+nSamp_anin = floor(1.0 * SampRate(aninmeta));
+
+% timing
+t0 = round(str2double(aninmeta.niSampRate)*240);
+n_samples_in_seconds = 840-240;
+t1 = round(str2double(aninmeta.niSampRate)*n_samples_in_seconds);
+aninArray = ReadBin(t0,t1,aninmeta,aninBin_fname,anin_path);
+
+%% OK, be careful in the channel re-ordering, please
+
+% the chanMap is the full 1:384 channel list in chronological order, and connected is the boolean
+% of which channels we actually used, and ch_struct.chMap are the
+% physically ordered channels.
+
+% 1) subset the physically ordered channels
+% this tells us which of the original channels exist, in which order
+phys_ch_subset = ch_struct.chMap(connected)';
+
+% 2) subset the chron channels
+chron_ch_subset = chanMap(connected);
+
+% 3) get sorting indices of the physical - thats it!
+[sorted_vals, sort_idx] = sort(phys_ch_subset);
+
+% 4) these are the indices of the original space that we used
+chs_original = chron_ch_subset(sort_idx);
+
+%three_spaces = [phys_ch_subset, chron_ch_subset, chs_original, [1:310]'];
+%disp(three_spaces);
+
+%% load the spike sorted output from the .npy files
+spike_times = readNPY('/userdata/lgwilliams/neuropixels/data/NP04/spike_sorted-lg/spike_times.npy');
+spike_clusters = readNPY('/userdata/lgwilliams/neuropixels/data/NP04/spike_sorted-lg/spike_clusters.npy');
+sua_label = readNPY('/userdata/lgwilliams/neuropixels/data/NP04/spike_sorted-lg/sua_label.npy');
+mua_label = readNPY('/userdata/lgwilliams/neuropixels/data/NP04/spike_sorted-lg/mua_label.npy');
+
+% derive params
+n_spike_events = length(spike_times);
+max_spike_time = max(spike_times);
+n_clusters = length(unique(spike_clusters));
+n_sua = length(sua_label);
+n_mua = length(mua_label);
+
+% because of the batching, we might end up with more time samples than we
+% asked for. we will see later how we want to deal with this. but for now,
+% lets just ignore any sample which is longer than 600 seconds.
+desired_max_tsamp = sfreq * n_samples_in_seconds;
+
+% 1) init the spike train matrix for SUA and MUA
+sua_spikes = zeros(n_sua, desired_max_tsamp);
+mua_spikes = zeros(n_mua, desired_max_tsamp);
+
+% 2) loop through each spike event and put into the correct bin
+for ev = 1:n_spike_events
+
+    % get time and label
+    s_time = spike_times(ev);
+    s_label = spike_clusters(ev);
+
+    % the batching issue - maybe lets fix this
+    if s_time < desired_max_tsamp
+
+    % get identity
+    idx_sua = find(sua_label == s_label);
+    idx_mua = find(mua_label == s_label);
+
+    % put into correct place
+    if sum(idx_sua) > 0
+        sua_spikes(idx_sua, s_time) = 1;
+    end
+    if sum(idx_mua) > 0
+        mua_spikes(idx_mua, s_time) = 1;
+    end
+    disp(s_time);
+
+    end
+end
+
+%% make into a struct
+
+new_fs = new_sfreq;
+
+for j = 1:length(evnt)
+    out(j).name = evnt(j).name;
+end
+
+for j = 1:length(evnt)
+    out_spikes(j).name = evnt(j).name;
+    out_sua(j).name = evnt(j).name;
+    out_mua(j).name = evnt(j).name;
+end
+
+% params
+tmin = 0.5;
+tmax = 1.5;
+nplot_samp = (tmax*new_sfreq) + (tmin*new_sfreq) + 1;
+n_plot_audsamp = (tmax*nSamp_anin) + (tmin*nSamp_anin) + 1;
+
+evoked = zeros(200, 308, nplot_samp);
+spike_ev = zeros(200, 308, 2*new_sfreq);
+spike_ds = zeros(200, 308, 2000);
+evoked_audio = zeros(200, n_plot_audsamp);
+corrcoefs = zeros(1, 200);
+for j = 1:length(evnt)
+
+    % print info
+    fprintf('%d ',j);
+    t1 = tic;
+
+    % epoch data in windows
+    d_wind = all_data_ds(:,round((evnt(j).CorrectStartTime-tmin)*new_fs):round((evnt(j).CorrectStartTime+tmax)*new_fs));
+    d_sent = all_data_ds(:,round((evnt(j).CorrectStartTime-tmin)*new_fs):round((evnt(j).StopTime+tmax)*new_fs));
+
+    % epoch data in windows
+    sua_sent = sua_spikes(:,round((evnt(j).CorrectStartTime-tmin)*new_fs):round((evnt(j).StopTime+tmax)*new_fs));
+    mua_sent = mua_spikes(:,round((evnt(j).CorrectStartTime-tmin)*new_fs):round((evnt(j).StopTime+tmax)*new_fs));
+
+    % epoch anin
+    anin_wind = aninArray(:,round((evnt(j).CorrectStartTime-tmin)*nSamp_anin):round((evnt(j).CorrectStartTime+tmax)*nSamp_anin));
+    anin_sent = aninArray(:,round((evnt(j).CorrectStartTime-tmin)*nSamp_anin):round((evnt(j).StopTime+tmax)*nSamp_anin));
+
+    % aud features
+    [env, peakRate, peakEnv] = find_peakRate(anin_sent(3, :), nSamp_anin);
+
+    % collect in struct
+    out(j).resp = d_sent(sort_idx, :);
+    out(j).tmin = -tmin; out(j).tmax = tmax;  % not symmetric anymore
+    out(j).np_sfreq = new_fs;
+    out(j).chs_original = chs_original;
+    out(j).chs_physical = sorted_vals;
+    out(j).anin = anin_sent;
+    out(j).anin_sfreq = nSamp_anin;
+    out(j).envelope = env;
+    out(j).peakRate = peakRate;
+    out(j).peakEnv = peakEnv;
+
+    % add zscore and spikes
+    %out_spikes(j).zscore_resp = zscore(double(out(j).resp)')';
+    zscore_d = zscore(double(out(j).resp)')';
+
+    % get spike data
+    evnt_thresh = 3; % 3 sd's
+    thresh_dat = abs(zscore_d);
+    thresh_dat = thresh_dat > evnt_thresh;
+    spk_data = zeros(size(thresh_dat));
+    for i = 1:size(thresh_dat,1)
+        tmp = findstr([0 thresh_dat(i,:)], [0 1]);
+        tmp(find(diff(tmp) <= 0.001*out(j).np_sfreq)) = [];
+        spk_data(i,tmp) = 1;
+    end
+    %out_spikes(j).spikes = spk_data;
+    spike_ev(j, :, :) = spk_data(:, 1:2*new_sfreq);
+
+    % get the downsampled
+    x_ds = resample(spk_data', 1, 10)';
+
+    % find spike w threshold
+    thresh = mean(max(x_ds) / 2);
+    x_ds_t = x_ds > thresh;
+    spike_ds(j, :, :) = x_ds_t(:, 1:2000);
+    out_spikes(j).spikes = x_ds_t;
+
+    % check correlation between number of spikes in orig and ds
+    rs = corrcoef(sum(spike_ev(j, :, :), 3), sum(spike_ds(j, :, :), 3));
+    corrcoefs(j) = rs(2);
+
+    % do the downsampling for the sorted spikes too
+    spk_data = zeros(size(sua_sent));
+    for i = 1:size(sua_sent, 1)
+        tmp = findstr([0 sua_sent(i,:)], [0 1]);
+        tmp(find(diff(tmp) <= 0.001*out(j).np_sfreq)) = [];
+        spk_data(i,tmp) = 1;
+    end
+
+    % get the downsampled
+    x_ds = resample(spk_data', 1, 10)';
+
+    % find spike w threshold
+    thresh = mean(max(x_ds) / 2);
+    x_ds_t = x_ds > thresh;
+    out_sua(j).spikes = x_ds_t;
+
+
+    % do the downsampling for the sorted spikes too
+    spk_data = zeros(size(mua_sent));
+    for i = 1:size(mua_sent, 1)
+        tmp = findstr([0 mua_sent(i,:)], [0 1]);
+        tmp(find(diff(tmp) <= 0.001*out(j).np_sfreq)) = [];
+        spk_data(i,tmp) = 1;
+    end
+
+    % get the downsampled
+    x_ds = resample(spk_data', 1, 10)';
+
+    % find spike w threshold
+    thresh = mean(max(x_ds) / 2);
+    x_ds_t = x_ds > thresh;
+    out_mua(j).spikes = x_ds_t;
+
+    % matrix
+    evoked(j, :, :) = zscore(double(d_wind(sort_idx, :)));
+    evoked_audio(j, :) = anin_wind(3, :);
+
+    % how long did that take?
+    toc(t1);
+end
+
+
+%%
+
+%save final results using v7 to load w python. have to use 7.3 for big
+%files
+fprintf('Saving final results in .mat file  \n')
+make_fname = strcat('ks_preproc_out_', string(new_sfreq), '.mat');
+fname = fullfile(rootZ, make_fname);
+save(fname, 'out', '-v7');
+
+make_fname = strcat('ks_preproc_spikes_', string(new_sfreq), '.mat');
+fname = fullfile(rootZ, make_fname);
+save(fname, 'out_spikes', '-v7');
+
+make_fname = strcat('lg-ks_sua_', string(new_sfreq), '.mat');
+fname = fullfile(rootZ, make_fname);
+save(fname, 'out_sua', '-v7');
+
+make_fname = strcat('lg-ks_mua_', string(new_sfreq), '.mat');
+fname = fullfile(rootZ, make_fname);
+save(fname, 'out_mua', '-v7');
+
+%% helper functions
+
+
+
+% =========================================================
+% Return sample rate as double.
+%
+function srate = SampRate(meta)
+    if strcmp(meta.typeThis, 'imec')
+        srate = str2double(meta.imSampRate);
+    else
+        srate = str2double(meta.niSampRate);
+    end
+end % SampRate
+
+function [meta] = ReadMeta(binName, path)
+
+    % Create the matching metafile name
+    [dumPath,name,dumExt] = fileparts(binName);
+    metaName = strcat(name, '.meta');
+
+    % Parse ini file into cell entries C{1}{i} = C{2}{i}
+    fid = fopen(fullfile(path, metaName), 'r');
+% -------------------------------------------------------------
+%    Need 'BufSize' adjustment for MATLAB earlier than 2014
+%    C = textscan(fid, '%[^=] = %[^\r\n]', 'BufSize', 32768);
+    C = textscan(fid, '%[^=] = %[^\r\n]');
+% -------------------------------------------------------------
+    fclose(fid);
+
+    % New empty struct
+    meta = struct();
+
+    % Convert each cell entry into a struct entry
+    for i = 1:length(C{1})
+        tag = C{1}{i};
+        if tag(1) == '~'
+            % remake tag excluding first character
+            tag = sprintf('%s', tag(2:end));
+        end
+        meta = setfield(meta, tag, C{2}{i});
+    end
+end % ReadMeta
+
+
+% =========================================================
+% Read nSamp timepoints from the binary file, starting
+% at timepoint offset samp0. The returned array has
+% dimensions [nChan,nSamp]. Note that nSamp returned
+% is the lesser of: {nSamp, timepoints available}.
+%
+% IMPORTANT: samp0 and nSamp must be integers.
+%
+function dataArray = ReadBin(samp0, nSamp, meta, binName, path)
+
+    nChan = str2double(meta.nSavedChans);
+
+    nFileSamp = str2double(meta.fileSizeBytes) / (2 * nChan);
+    samp0 = max(samp0, 0);
+    nSamp = min(nSamp, nFileSamp - samp0);
+
+    sizeA = [nChan, nSamp];
+
+    fid = fopen(fullfile(path, binName), 'rb');
+    fseek(fid, samp0 * 2 * nChan, 'bof');
+    dataArray = fread(fid, sizeA, 'int16=>double');
+    fclose(fid);
+end % ReadBin
