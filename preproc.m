5cd03ec414cd9b9bdb9cfc64dc2c05106c467b6a
Laura Gwilliams <laura.gwilliams@ucsf.edu>
2021-05-04 15:39:43 -0700

    update preproc

--- preproc.m
+++ preproc.m
@@ -1,31 +1,59 @@
-%% define paths
+%% define params
 
-sys_flag = 'matlab'; % 'matlab', 'python' - which language will you use?
+% branch decisions
+sys_flag = 'python'; % 'matlab', 'python' - which language will you use?
+user = 'lg';
 
-addpath(genpath('/home/matt_l/matlab/npy-matlab/'))
-% addpath(genpath('/home/matt_l/matlab/KS_LG_MKL/')) % path to kilosort folder
-addpath(genpath('/home/matt_l/matlab/KS-2.5'))
-addpath(genpath('/home/matt_l/matlab/neuropixels/')) % path to kilosort folder
-addpath(genpath('/home/matt_l/matlab/TIMIT')) % path to timit preproc
-
-timitPath = '/home/matt_l/matlab/TIMIT';
-rootZ = '/userdata/matt_l/neuropixels/NP04/raw/NP04_B2_g0/NP04_B2_g0_imec0/'; % the raw data binary file is in this folder
-rootH = rootZ; % path to temporary binary file (same size as data, should be on fast SSD)
-pathToYourConfigFile = '/home/matt_l/matlab/KS_LG_MKL/configFiles'; % take from Github folder and put it somewhere else (together with the master_file)
-chanMapFile = 'Intraop1_g0_t0.imec0.ap_kilosortChanMap.mat'; %'neuropixPhase3B2_kilosortChanMap.mat';
-
-%% params
+% data curation params
 ops.trange    = [240 840]; % time range to sort
 ops.NchanTOT  = 385; % total number of channels in your recording
+decimation = 10; % this is for the spikes
 
-% filtering
-%ops.fslow = 10000;
-%ops.fshigh = 300;
 
-%% deriv paths
+%% paths
+
+% laura's paths:
+switch user
+    % Laura's paths
+    case 'lg'
+        addpath(genpath('/userdata/lgwilliams/neuropixels/software/npy-matlab-master/npy-matlab/'))
+        addpath(genpath('/userdata/lgwilliams/neuropixels/software/')) % path to kilosort folder
+        addpath(genpath('/userdata/lgwilliams/neuropixels/')) % path to kilosort folder
+        rootZ = '/userdata/lgwilliams/neuropixels/data/NP04/raw/NP04_B2_g0/NP04_B2_g0_imec0/'; % the raw data binary file is in this folder
+        rootH = rootZ; % path to temporary binary file (same size as data, should be on fast SSD)
+        pathToYourConfigFile = '/userdata/lgwilliams/neuropixels/software/KS-2.5/configFiles'; % take from Github folder and put it somewhere else (together with the master_file)
+        timitPath = '/home/lgwilliams/matlab/TIMIT';
+        addpath(genpath(timitPath));
+        ks_output_dir = '/userdata/lgwilliams/neuropixels/data/NP04/spike_sorted-lg';
+    
+    % Matt's paths
+    case 'mkl'
+        addpath(genpath('/home/matt_l/matlab/npy-matlab/'))
+        addpath(genpath('/home/matt_l/matlab/KS_LG_MKL/')) % path to kilosort folder
+        addpath(genpath('/home/matt_l/matlab/KS-2.5'))
+        addpath(genpath('/home/matt_l/matlab/neuropixels/')) % path to kilosort folder
+        addpath(genpath('/home/matt_l/matlab/TIMIT')) % path to timit preproc
+
+        timitPath = '/home/matt_l/matlab/TIMIT';
+        rootZ = '/userdata/matt_l/neuropixels/NP04/raw/NP04_B2_g0/NP04_B2_g0_imec0/'; % the raw data binary file is in this folder
+        rootH = rootZ; % path to temporary binary file (same size as data, should be on fast SSD)
+        pathToYourConfigFile = '/home/matt_l/matlab/KS_LG_MKL/configFiles'; % take from Github folder and put it somewhere else (together with the master_file)
+end
+
+% regardless of user:
+chanMapFile = 'Intraop1_g0_t0.imec0.ap_kilosortChanMap.mat'; %'neuropixPhase3B2_kilosortChanMap.mat';
+anin_path = '/userdata/matt_l/neuropixels/NP04/raw/NP04_B2_g0';
+aninBin_fname = 'NP04_B2_g0_t0.nidq.bin';
+aninMeta_fname = 'NP04_B2_g0_t0.nidq.meta';
+
+% deriv paths
 run(fullfile(pathToYourConfigFile, 'configFile384.m'))
 ops.fproc   = fullfile(rootH, 'temp_wh.dat'); % proc file on a fast SSD
 ops.chanMap = fullfile(pathToYourConfigFile, chanMapFile);
+
+% define path to spike data
+spike_times_fname = fullfile(ks_output_dir, 'spike_times.npy');
+spike_clusters_fname = fullfile(ks_output_dir, 'spike_clusters.npy');
 
 %% load params
 
@@ -40,6 +68,8 @@ switch sys_flag
         evnt = events.evnt;
         evnt_orig = load(fullfile(rootZ, 'NP04_evnt.mat'));
         evnt_orig = evnt_orig.evnt;
+        
+        % correct the timing?
         for i = 1:length(evnt)
             tdiff(i) = evnt_orig(i).StartTime - evnt(i).StartTime;
         end
@@ -96,19 +126,13 @@ ops.fbinary = fullfile(rootZ, fs(1).name);
 
 % preprocess data to create temp_wh.dat
 sfreq = 30000;
-decimate = 1;
-new_sfreq = sfreq/decimate;
-
-% change the NT to be an integer of the resulting sfreq
-ops.NT = sfreq*2;
+new_sfreq = sfreq;
+spk_fs_after_decim = sfreq / decimation;
 
 % get data
 all_data_ds = preprocessData_returnMatrix(ops);
 
 %% load anin
-anin_path = '/userdata/matt_l/neuropixels/NP04/raw/NP04_B2_g0';
-aninBin_fname = 'NP04_B2_g0_t0.nidq.bin';
-aninMeta_fname = 'NP04_B2_g0_t0.nidq.meta';
 
 % load it
 aninmeta = ReadMeta(aninMeta_fname, anin_path);
@@ -145,12 +169,62 @@ chs_original = chron_ch_subset(sort_idx);
 %disp(three_spaces);
 
 %% load the spike sorted output from the .npy files
-spike_times = readNPY('/userdata/lgwilliams/neuropixels/data/NP04/spike_sorted-lg/spike_times.npy');
-spike_clusters = readNPY('/userdata/lgwilliams/neuropixels/data/NP04/spike_sorted-lg/spike_clusters.npy');
-sua_label = readNPY('/userdata/lgwilliams/neuropixels/data/NP04/spike_sorted-lg/sua_label.npy');
-mua_label = readNPY('/userdata/lgwilliams/neuropixels/data/NP04/spike_sorted-lg/mua_label.npy');
+%spike_times = readNPY('/userdata/lgwilliams/neuropixels/data/NP04/spike_sorted-lg/spike_times.npy');
+%spike_clusters = readNPY('/userdata/lgwilliams/neuropixels/data/NP04/spike_sorted-lg/spike_clusters.npy');
 
-% derive params
+% read in the files
+spike_times = readNPY(spike_times_fname);
+spike_clusters = readNPY(spike_clusters_fname);
+
+% read in the cluster labels and note the sua and mua cluster ids
+mua_label = [];
+sua_label = [];
+cluster_info = table2struct(readtable(fullfile(ks_output_dir, 'cluster_info.csv')));
+
+% init
+sua_counter = 1; mua_counter = 1;
+clear sua_info
+clear mua_info
+
+% loop
+for row_i = 1:length(cluster_info)
+    
+    % is this a sua cluster?
+    if length(cluster_info(row_i).final) > 0
+        sua_label = [sua_label, cluster_info(row_i).id];
+        
+        % pull out all the info from the cluster csv
+        % OK this code is so ugly plz fix
+        sua_info(sua_counter).id = cluster_info(row_i).id;        
+        sua_info(sua_counter).depth = cluster_info(row_i).depth;
+        sua_info(sua_counter).fr = cluster_info(row_i).fr;
+        sua_info(sua_counter).ch = cluster_info(row_i).ch;
+        sua_info(sua_counter).amp = cluster_info(row_i).Amplitude;
+        sua_info(sua_counter).n_spikes = cluster_info(row_i).n_spikes;
+        sua_info(sua_counter).group = 'sua';
+        sua_counter = sua_counter + 1;
+   
+    % otherwise, is this a mua cluster?
+    % ok this is a dumb way but i'm not sure how else -- 3 chars = "MUA"
+    elseif length(cluster_info(row_i).group) == 3
+        mua_label = [mua_label, cluster_info(row_i).id];
+
+        % pull out all the info from the cluster csv
+        % OK this code is so ugly plz fix
+        mua_info(mua_counter).id = cluster_info(row_i).id;        
+        mua_info(mua_counter).depth = cluster_info(row_i).depth;
+        mua_info(mua_counter).fr = cluster_info(row_i).fr;
+        mua_info(mua_counter).ch = cluster_info(row_i).ch;
+        mua_info(mua_counter).amp = cluster_info(row_i).Amplitude;
+        mua_info(mua_counter).n_spikes = cluster_info(row_i).n_spikes;
+        mua_info(mua_counter).group = 'mua';
+        mua_counter = mua_counter + 1;        
+        
+    end
+end
+
+
+%% derive params
 n_spike_events = length(spike_times);
 max_spike_time = max(spike_times);
 n_clusters = length(unique(spike_clusters));
@@ -215,26 +289,41 @@ spike_ev = zeros(length(evnt), size(all_data_ds,1), 2*new_sfreq);
 spike_ds = zeros(length(evnt), size(all_data_ds,1), 2000);
 evoked_audio = zeros(length(evnt), n_plot_audsamp);
 corrcoefs = zeros(1, length(evnt));
-for j = 1:length(evnt)
+for j = 1:3 %1:length(evnt)
+    
+    % get tmin and tmax for this sentence
+    sec_start = evnt(j).CorrectStartTime;
+    sec_stop = evnt(j).StopTime;
+    
+    % get sentence duration
+    sec_dur = sec_stop - sec_start;
+    
+    % descrepency between wav start and phoneme start
+    this_tdiff = evnt(j).CorrectStartTime - evnt(j).StartTime;
+    
+    % pad start and stop
+    pad_start = sec_start - tmin;
+    pad_stop = sec_stop + tmax;
 
     % print info
     fprintf('%d ',j);
     timerStart = tic;
 
-    % epoch data in windows
-%     d_wind = all_data_ds(:,round((evnt(j).CorrectStartTime-tmin)*new_fs):round((evnt(j).CorrectStartTime+tmax)*new_fs));
-    d_sent = all_data_ds(:,round((evnt(j).CorrectStartTime-tmin)*new_fs):round((evnt(j).StopTime+tmax)*new_fs));
+    % epoch data around sentence
+    d_sent = all_data_ds(:,round((pad_start)*new_fs):round((pad_stop)*new_fs));
 
     % epoch data in windows
-    sua_sent = spikes_sua(:,round((evnt(j).CorrectStartTime-tmin)*new_fs):round((evnt(j).StopTime+tmax)*new_fs));
-    mua_sent = spikes_mua(:,round((evnt(j).CorrectStartTime-tmin)*new_fs):round((evnt(j).StopTime+tmax)*new_fs));
+    sua_sent = spikes_sua(:,round((pad_start)*new_fs):round((pad_stop)*new_fs));
+    mua_sent = spikes_mua(:,round((pad_start)*new_fs):round((pad_stop)*new_fs));
 
     % collect in struct
-    out(j).resp = d_sent; %d_sent(sort_idx, :);
-    out(j).tmin = -tmin; out(j).tmax = tmin;  % symmetric 
-    out(j).dataf = new_fs;
+    %out(j).resp = d_sent; %d_sent(sort_idx, :);
+    out(j).tmin = -tmin; out(j).tmax = tmax;  % not necc symmetric 
+    %out(j).dataf = new_fs;
+    out(j).spike_fs = spk_fs_after_decim;
     out(j).chs_original = chs_original;
     out(j).chs_physical = sorted_vals;
+    out(j).duration = sec_dur;
     
     fprintf(1,'Loading %s\n',evnt(j).wname);
     [t1,fw] = audioread(evnt(j).wname); % load audio file of sound
@@ -248,32 +337,47 @@ for j = 1:length(evnt)
 %     fw = 16000;
         
     % epoch anin
-%     anin_wind = aninArray(:,round((evnt(j).CorrectStartTime-tmin)*nSamp_anin):round((evnt(j).CorrectStartTime+tmax)*nSamp_anin));
-    anin_sent = aninArray(:,round((evnt(j).CorrectStartTime-tmin)*nSamp_anin):round((evnt(j).StopTime+tmax)*nSamp_anin));
+    anin_sent = aninArray(:,round((pad_start)*nSamp_anin):round((pad_stop)*nSamp_anin));
 
     % aud features
     [env, peakRate, peakEnv] = find_peakRate(anin_sent(3, :), nSamp_anin);
+    
+    % what is the sample rate of these aud features?
+    ratio = size(env, 1) / size(anin_sent, 2);
+    peakRate_fs = ceil(nSamp_anin * ratio);
+    
+    % resample env and peakRate features to match the rest
+    % resampling the binary is not super great - is taking the abs the
+    % best?
+    env_rs = resample(env, spk_fs_after_decim, peakRate_fs);
+    peakRate_rs = abs(resample(peakRate, spk_fs_after_decim, peakRate_fs));
+    peakEnv_rs = abs(resample(peakEnv, spk_fs_after_decim, peakRate_fs));
 
-    % Add zero padding on either side as appropriate
-    t1 = [zeros(tmin*fw,1); t1(ceil(abs(tdiff(j))*fw):end) ;zeros(tmin*fw,1)];
+    % Add zero padding to waveform either side as appropriate
+    t1 = [zeros(tmin*fw,1); t1(ceil(abs(this_tdiff)*fw):end) ;zeros(tmax*fw,1)];
     t1 = resample(t1,new_fs,fw);
-    out(j).soundf = fw;
-    out(j).sound = t1;
+
+    % spectrogram
     wintime = 0.025;
     steptime = 1/fw;
     pspectrum = powspec(t1, fw, wintime, steptime);
     aspectrum = audspec(pspectrum, fw, 80, 'mel');
     aud = aspectrum.^( 0.077);
+    
+    % add to the struct
+    out(j).soundf = fw;
+    out(j).sound = t1;
     out(j).aud  = aud;
     out(j).duration = length(t1)/fw;
-
-    out(j).envelope = env;
-    out(j).peakRate = peakRate;
-    out(j).peakEnv = peakEnv;
+    
+    % add these aud features - they have been resampled to match spike rate
+    out(j).envelope = env_rs;
+    out(j).peakRate = peakRate_rs;
+    out(j).peakEnv = peakEnv_rs;
 
     % add zscore and spikes
     %out_spikes(j).zscore_resp = zscore(double(out(j).resp)')';
-    zscore_d = zscore(double(out(j).resp)')';
+    zscore_d = zscore(double(d_sent)')';
 
     % get spike data
     evnt_thresh = 3; % 3 sd's
@@ -282,52 +386,51 @@ for j = 1:length(evnt)
     spk_data = zeros(size(thresh_dat));
     for i = 1:size(thresh_dat,1)
         tmp = findstr([0 thresh_dat(i,:)], [0 1]);
-        tmp(find(diff(tmp) <= 0.001*out(j).dataf)) = [];
+        tmp(find(diff(tmp) <= 0.001*new_fs)) = [];
         spk_data(i,tmp) = 1;
     end
     %out_spikes(j).spikes = spk_data;
-    spike_ev(j, :, :) = spk_data(:, 1:2*new_sfreq);
+    %spike_ev(j, :, :) = spk_data(:, 1:2*new_sfreq);
 
     % get the downsampled
-    x_ds = resample(spk_data', 1, 10)';
+    x_ds = resample(spk_data', 1, decimation)';
 
     % find spike w threshold
     thresh = mean(max(x_ds) / 2);
     x_ds_t = x_ds > thresh;
-    spike_ds(j, :, :) = x_ds_t(:, 1:2000);
+    %spike_ds(j, :, :) = x_ds_t(:, 1:2000);
     out(j).spikes = x_ds_t;
 
     % check correlation between number of spikes in orig and ds
-    rs = corrcoef(sum(spike_ev(j, :, :), 3), sum(spike_ds(j, :, :), 3));
-    corrcoefs(j) = rs(2);
+    %rs = corrcoef(sum(spike_ev(j, :, :), 3), sum(spike_ds(j, :, :), 3));
+    %corrcoefs(j) = rs(2);
 
     % do the downsampling for the sorted spikes too
     spk_data = zeros(size(sua_sent));
     for i = 1:size(sua_sent, 1)
         tmp = findstr([0 sua_sent(i,:)], [0 1]);
-        tmp(find(diff(tmp) <= 0.001*out(j).dataf)) = [];
+        tmp(find(diff(tmp) <= 0.001*new_fs)) = [];
         spk_data(i,tmp) = 1;
     end
 
     % get the downsampled
-    x_ds = resample(spk_data', 1, 10)';
+    x_ds = resample(spk_data', 1, decimation)';
 
     % find spike w threshold
     thresh = mean(max(x_ds) / 2);
     x_ds_t = x_ds > thresh;
     out(j).spikes_sua = x_ds_t;
 
-
     % do the downsampling for the sorted spikes too
     spk_data = zeros(size(mua_sent));
     for i = 1:size(mua_sent, 1)
         tmp = findstr([0 mua_sent(i,:)], [0 1]);
-        tmp(find(diff(tmp) <= 0.001*out(j).dataf)) = [];
+        tmp(find(diff(tmp) <= 0.001*new_fs)) = [];
         spk_data(i,tmp) = 1;
     end
 
     % get the downsampled
-    x_ds = resample(spk_data', 1, 10)';
+    x_ds = resample(spk_data', 1, decimation)';
 
     % find spike w threshold
     thresh = mean(max(x_ds) / 2);
@@ -342,13 +445,16 @@ for j = 1:length(evnt)
     toc(timerStart);
 end
 
+% add sua and mua info to the out 
+out(1).sua_info = sua_info;
+out(1).mua_info = mua_info;
 
 %%
 
 %save final results using v7 to load w python. have to use 7.3 for big
 %files
 fprintf('Saving final results in .mat file  \n')
-make_fname = strcat('ks_preproc_out_', string(new_sfreq), '.mat');
+make_fname = strcat('ks_preproc_out_', string(spk_fs_after_decim), '.mat');
 fname = fullfile(rootZ, make_fname);
 save(fname, 'out', '-v7.3');
 
